3253c2687b3269d82fa94db05dc2e0e40707bddb
LiShaojun1126 <yur1loverslave@gmail.com>
2025-06-07 13:43:11 -0400

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,6 +33,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>hello</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
